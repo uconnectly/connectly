5b0707bdf276f8eb39b942061de2937a400e6ee9
uconnectly <uttamkasana001@gmail.com>
2026-01-22 12:47:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
